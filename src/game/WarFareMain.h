79963736d6685b33b9e24ec33afc236408085ea5
Steve Williams <90905675+stevewgr@users.noreply.github.com>
2022-07-17 14:33:06 -0700

    Comment out existing definitions from WinSDK.

--- src/game/WarFareMain.h
+++ src/game/WarFareMain.h
@@ -7,14 +7,14 @@ LRESULT CALLBACK OYBLowLevelKeyboardProc (INT nCode, WPARAM wParam, LPARAM lPara
 
 #define WH_KEYBOARD				2
 #define WH_KEYBOARD_LL			13
-#define LLKHF_ALTDOWN			0x00000020
+//#define LLKHF_ALTDOWN			0x00000020
 
-typedef struct tagKBDLLHOOKSTRUCT {
-    DWORD vkCode;      // virtual key code
-    DWORD scanCode;    // scan code
-    DWORD flags;       // flags
-    DWORD time;        // time stamp for this message
-    DWORD dwExtraInfo; // extra info from the driver or keybd_event
-} KBDLLHOOKSTRUCT, FAR *LPKBDLLHOOKSTRUCT, *PKBDLLHOOKSTRUCT;
+//typedef struct tagKBDLLHOOKSTRUCT {
+//    DWORD vkCode;      // virtual key code
+//    DWORD scanCode;    // scan code
+//    DWORD flags;       // flags
+//    DWORD time;        // time stamp for this message
+//    DWORD dwExtraInfo; // extra info from the driver or keybd_event
+//} KBDLLHOOKSTRUCT, FAR *LPKBDLLHOOKSTRUCT, *PKBDLLHOOKSTRUCT;
 
 #endif //_WARFAREMAIN_H_
