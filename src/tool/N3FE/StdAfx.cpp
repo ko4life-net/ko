d72f0622172ed4559b7c742ef100723c94c9d7d1
Steve Williams <90905675+stevewgr@users.noreply.github.com>
2022-07-21 13:50:09 -0700

    Remove N3FE from tools.
    Looks like an older version of N3FXE.
    
    No need it here.

--- src/tool/N3FE/StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	N3FE.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-
