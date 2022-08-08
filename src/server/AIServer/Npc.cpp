6ddad3dba9bf4bda18d9a8b2579242ee80be04bc
Steve Williams <90905675+stevewgr@users.noreply.github.com>
2022-08-08 16:24:00 -0700

    Fix AIServer not sending sid.
    Note that Ebenezer was trying to read this, whilst AIServer didn't send
    it.

--- src/server/AIServer/Npc.cpp
+++ src/server/AIServer/Npc.cpp
@@ -4344,6 +4344,7 @@ void CNpc::FillNpcInfo(char *temp_send, int &index, BYTE flag)
 	else
 		SetByte(temp_send, 1, index );					// region에 등록		
 	SetShort(temp_send, m_sNid+NPC_BAND, index );
+	SetShort(temp_send, m_sSid, index);
 	SetShort(temp_send, m_sPid, index );
 	SetShort(temp_send, m_sSize, index );
 	SetInt(temp_send, m_iWeapon_1, index );
@@ -4378,6 +4379,7 @@ void CNpc::SendNpcInfoAll(char *temp_send, int &index, int count)
 	else
 		SetByte(temp_send, 1, index );					// region에 등록		
 	SetShort(temp_send, m_sNid+NPC_BAND, index );
+	SetShort(temp_send, m_sSid, index);
 	SetShort(temp_send, m_sPid, index );
 	SetShort(temp_send, m_sSize, index );
 	SetInt(temp_send, m_iWeapon_1, index );
