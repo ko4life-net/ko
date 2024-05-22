4e6018cb5a92c8730b2cc4352a29a3e8751953d1
Steve Williams <90905675+stevewgr@users.noreply.github.com>
2024-05-22 18:15:01 -0400

    Bump db version from 1.0.2 to 1.1.0.

--- config.cmd
+++ config.cmd
@@ -17,7 +17,7 @@ rmdir /s /q %CURRENT_PATH%src\db 2> NUL
 
 git clone --depth=1 https://github.com/ko4life-net/ko-assets %CURRENT_PATH%src\assets
 git clone --depth=1 --branch=1.0.0 https://github.com/ko4life-net/ko-vendor %CURRENT_PATH%src\vendor
-git clone --branch=1.0.2 https://github.com/ko4life-net/ko-db %CURRENT_PATH%src\db
+git clone --branch=1.1.0 https://github.com/ko4life-net/ko-db %CURRENT_PATH%src\db
 
 @REM src\All.sln
 
@@ -27,11 +27,8 @@ echo Configuring done.
 echo:
 echo You may want to to start the src\All.sln Visual Studio solution to start developing.
 echo:
-echo If you haven't setup the database just yet, one last manual step is needed (assuming you have any version of MS-SQL server installed):
-echo   1. Run the powershell script under src\db\import.ps1 to create and import the database into your SQL server.
-echo     1.1. If you installed your SQL server as Named Instance, you may need to modify the server name in the script to yours.
-echo   2. Setup odbcad as usual targeting kodb database name and your SQL server so that the server files can access the database.
-echo     2.1. Note that you don't need to mess with the default configurations just to keep things simple.
+echo If you haven't setup the database just yet, one last manual step is needed (assuming you have any version of Microsoft SQL Server installed):
+echo   - Run the powershell script under src\db\import.ps1 to create and import the database into your SQL server.
 echo:
 echo:
 echo Thank you and happy coding!
@@ -39,13 +36,3 @@ echo:
 echo:
 
 pause
-
-@REM Here is registry an example of my current odbcad settings:
-@REM [HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\ODBC\ODBC.INI\kodb]
-@REM "Driver"="c:\\Windows\\SysWOW64\\sqlncli10.dll"
-@REM "Server"="STEVEW\\SQLEXPRESS2008"
-@REM "Database"="kodb"
-@REM "Language"="us_english"
-@REM "LastUser"="User"
-@REM "Trusted_Connection"="Yes"
-@REM "AutoTranslate"="No"
