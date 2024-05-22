834cbc5d599aee649786f504d69c0d93c1970469
Steve Williams <90905675+stevewgr@users.noreply.github.com>
2024-05-21 21:37:31 -0400

    Bump db version from 1.0.0 to 1.0.2.

--- config.cmd
+++ config.cmd
@@ -17,7 +17,7 @@ rmdir /s /q %CURRENT_PATH%src\db 2> NUL
 
 git clone --depth=1 https://github.com/ko4life-net/ko-assets %CURRENT_PATH%src\assets
 git clone --depth=1 --branch=1.0.0 https://github.com/ko4life-net/ko-vendor %CURRENT_PATH%src\vendor
-git clone --branch=1.0.0 https://github.com/ko4life-net/ko-db %CURRENT_PATH%src\db
+git clone --branch=1.0.2 https://github.com/ko4life-net/ko-db %CURRENT_PATH%src\db
 
 @REM src\All.sln
 
