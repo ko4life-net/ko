7e1947f63e52daa05004899ad20fd75162c3019b
Steve Williams <90905675+stevewgr@users.noreply.github.com>
2024-06-05 22:23:21 -0400

    Update execution policy for current user.
    This will allow users to run powershell scripts, since many folks were
    wondering why they can't run the powershell scripts on a freshly
    installed Windows machine.

--- config.cmd
+++ config.cmd
@@ -1,8 +1,10 @@
-@REM Script for noobs that have hard time reading instructions.
 @echo off
 
 echo Configuring started...
 set CURRENT_PATH=%~dp0
+
+echo Allowing current user to run powershell scripts...
+powershell -command "Set-ExecutionPolicy Bypass -Scope CurrentUser"
 
 where /q git
 if ERRORLEVEL 1 (
@@ -27,8 +29,8 @@ echo Configuring done.
 echo:
 echo You may want to to start the src\All.sln Visual Studio solution to start developing.
 echo:
-echo If you haven't setup the database just yet, one last manual step is needed (assuming you have any version of Microsoft SQL Server installed):
-echo   - Run the powershell script under src\db\import.ps1 to create and import the database into your SQL server.
+echo If you haven't setup the database just yet, one last manual step is needed (assuming you have any version of Microsoft SQL Server installed).
+echo Run the powershell script under src\db\import.ps1 to setup the database for you.
 echo:
 echo:
 echo Thank you and happy coding!
