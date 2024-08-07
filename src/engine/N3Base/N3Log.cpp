c620f097ee2693929018374a875f19f220ce9569
Steve Williams <90905675+stevewgr@users.noreply.github.com>
2024-08-06 20:43:55 -0400

    Refactor logging configuration for flexibility and performance.
    This class is used across many projects with different requirements.
    For instance, not all projects need spdlog as a dependency; some
    only require the N3Base engine, such as our tools. Thus, it made
    sense to make this implementation more configurable for consumers
    using preprocessor directives.
    
    With the introduced macros, no code will be generated when logs
    are disabled in release mode. While this approach may make the
    code look less elegant, it provides necessary flexibility and
    efficiency for its various use-cases.

--- src/engine/N3Base/N3Log.cpp
+++ src/engine/N3Base/N3Log.cpp
@@ -2,13 +2,15 @@
 
 #include "N3Log.h"
 
+#if defined(N3LOG_ENABLE_SPDLOG) && !defined(N3LOG_DISABLE_LOGS)
 #include <spdlog/sinks/stdout_color_sinks.h>
 #include <spdlog/sinks/basic_file_sink.h>
 
 std::shared_ptr<spdlog::logger> CN3Log::s_Logger;
+#endif // #if defined(N3LOG_ENABLE_SPDLOG) && !defined(N3LOG_DISABLE_LOGS)
 
-void CN3Log::Init(const std::string & loggerName) {
-#ifdef _DEBUG
+void CN3Log::Init(const std::string & szLoggerName) {
+#if !defined(N3LOG_DISABLE_LOGS)
     AllocConsole();
 
     // Position console window at the top right corner
@@ -24,27 +26,31 @@ void CN3Log::Init(const std::string & loggerName) {
     const int iY = iPadding;
     SetWindowPos(hConsole, HWND_TOP, iX, iY, iTermWidth, iTermHeight, SWP_NOACTIVATE | SWP_SHOWWINDOW);
 
+    // Redirect outputs
     FILE * fDummy;
     freopen_s(&fDummy, "CONOUT$", "w", stdout);
     freopen_s(&fDummy, "CONOUT$", "w", stderr);
     freopen_s(&fDummy, "CONIN$", "r", stdin);
+#if defined(N3LOG_ENABLE_SPDLOG)
+    std::vector<spdlog::sink_ptr> vLogSinks;
+    vLogSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+    vLogSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>(szLoggerName + ".log", true));
+    vLogSinks[0]->set_pattern("%^[%T] %n: %v%$");
+    vLogSinks[1]->set_pattern("[%T] [%l] %n: %v");
 
-    std::vector<spdlog::sink_ptr> logSinks;
-    logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
-    logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>(loggerName + ".log", true));
-    logSinks[0]->set_pattern("%^[%T] %n: %v%$");
-    logSinks[1]->set_pattern("[%T] [%l] %n: %v");
-
-    s_Logger = std::make_shared<spdlog::logger>(loggerName, begin(logSinks), end(logSinks));
+    s_Logger = std::make_shared<spdlog::logger>(szLoggerName, begin(vLogSinks), end(vLogSinks));
     s_Logger->set_level(spdlog::level::trace);
     s_Logger->flush_on(spdlog::level::trace);
     spdlog::register_logger(s_Logger);
-#endif
+#endif // #if defined(N3LOG_ENABLE_SPDLOG)
+#endif // #if !defined(N3LOG_DISABLE_LOGS)
 }
 
 void CN3Log::Destroy() {
-#ifdef _DEBUG
+#if !defined(N3LOG_DISABLE_LOGS)
+#if defined(N3LOG_ENABLE_SPDLOG)
     spdlog::shutdown();
+#endif // #if defined(N3LOG_ENABLE_SPDLOG)
     FreeConsole();
-#endif
+#endif // #if !defined(N3LOG_DISABLE_LOGS)
 }
